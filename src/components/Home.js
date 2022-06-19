cd800127f994afaf88f1970353eb3243de03a816
Karlo <94782031+Kaelo-Brandon-Mokalake@users.noreply.github.com>
2022-06-19 23:15:28 +0200

    Portfolio

---
+++ src/components/Home.js
@@ -0,0 +1,105 @@
+const Home = () => {
+  return (
+    <>
+      <div className="sidebar">
+        <nav class="navbar bg-light">
+          <a href="">Home</a>
+        </nav>
+      </div>
+
+      <div className="name">
+        <h1>HI, I'M KAELO B. MOKALAKE</h1>
+      </div>
+
+      <div className="me">
+        <img src="./images/ph.png" alt="" width="1080px" height="1020px" />
+      </div>
+
+      <div className="about">
+        <h2>About me</h2>
+        <hr />
+
+        <p>
+          I'm a recent HyperionDev Software Engineer Graduate and a self-driven
+          innovative individual who always seeks new challenges to take on. I'm
+          a very honest and open person that love helping others in anything
+          that they find difficult. I have self-taught development background
+          through various online trainings and platforms, these include
+          Microsoft Azure and AWS. I like to set deadlines and meet them and
+          this gives me a sense of accomplishment and I love creating an
+          organised schedule for completing tasks on time. I would definetily
+          want to develop innovative solutions that solves many problems that my
+          country is facing and the world at large.
+        </p>
+        <br />
+        <p>
+          Being a developer was always my passion growing up. While i was
+          browsing through the internet looking for opportunities i came across
+          a <b>SovTech</b> post about launching the second Graduate Programme. I
+          researched a bit about the company and i found out that it's an
+          amazing place to be at. As a leading innovative tech institute there
+          is no way that it could have gotten this far without providing a
+          platform for people to grow in. I want to be part of a team that
+          always excels and that care about its people and hope to join the
+          platform as well.
+        </p>
+
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <h2>Skills</h2>
+        <hr />
+
+        <ul>
+          <li>JAVA</li>
+          <li>PYTHON</li>
+          <li>MACHINE LEARNING</li>
+          <li>MS AZURE</li>
+          <li>AWS</li>
+        </ul>
+
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <h2>Certificate & Qualifications</h2>
+        <hr />
+        <ul>
+          <li>SOFTWARE ENGINEER BOOTCAMP Certificate</li>
+          <li>MS AZURE FUNDAMENTALS Certificate</li>
+          <li>MS Azure AI FUNDAMENTALS Certificate</li>
+          <li>Java SE8 PROGRAMMER Certificate</li>
+          <li>NQF LEVEL 4 Certificate</li>
+        </ul>
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <h2>Contact me</h2>
+        <hr />
+
+        <div className="social">
+          <a href="mailto:mokalake666@gmail.com">email</a>
+
+          <a
+            href="https://www.linkedin.com/in/kaelo-brandon-mokalake-2ba7321a9/"
+            target="_blank"
+          >
+            LinkendIn
+          </a>
+        </div>
+
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </>
+  );
+};
+
+export default Home;
